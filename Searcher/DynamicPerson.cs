81a5dd0fe7946cba44a3a084b4b606e625209ce2
Danish <danishpa@gmail.com>
2015-08-21 15:59:34 +0300

    Moved generic code to another class

--- Searcher/DynamicPerson.cs
+++ Searcher/DynamicPerson.cs
@@ -9,51 +9,6 @@ using System.Collections.ObjectModel;
 
 namespace Searcher
 {
-    public class DynamicPropertyDescriptor : PropertyDescriptor
-    {
-        public DynamicPropertyDescriptor(string name)
-            : base(name, null)
-        {
-        }
-
-        public override Type ComponentType
-        {
-            get { return typeof(object); }
-        }
-
-        public override bool IsReadOnly
-        {
-            get { return false; }
-        }
-
-        public override Type PropertyType
-        {
-            get { return typeof(object); }
-        }
-
-        public override bool CanResetValue(object component)
-        {
-            return false;
-        }
-
-        public override void ResetValue(object component)
-        {
-        }
-
-        public override bool ShouldSerializeValue(object component)
-        {
-            return false;
-        }
-        public override object GetValue(object component)
-        {
-            throw new NotImplementedException();
-        }
-        public override void SetValue(object component, object value)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class DynamicPerson : DynamicObject, ISearchable
     {
         private string[] _headers;
@@ -92,37 +47,6 @@ namespace Searcher
         public override IEnumerable<string> GetDynamicMemberNames()
         {
             return _headers;
-        }
-    }
-
-    public class DynamicList<T> : List<T>, ITypedList
-        where T : DynamicObject
-    {
-        public DynamicList(IEnumerable<T> collection)
-            : base(collection)
-        {
-        }
-
-        public PropertyDescriptorCollection GetItemProperties(PropertyDescriptor[] listAccessors)
-        {
-            DynamicPropertyDescriptor[] dynamicDescriptors = { };
-
-            if (this.Any())
-            {
-                var firstItem = this[0];
-
-                dynamicDescriptors =
-                    firstItem.GetDynamicMemberNames()
-                    .Select(p => new DynamicPropertyDescriptor(p))
-                    .ToArray();
-            }
-
-            return new PropertyDescriptorCollection(dynamicDescriptors);
-        }
-
-        public string GetListName(PropertyDescriptor[] listAccessors)
-        {
-            return null;
         }
     }
 }
