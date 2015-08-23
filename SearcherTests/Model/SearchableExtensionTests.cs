3d9f7b279a9ac70d9e00917cfb74db0815abdbce
Danish <danishpa@gmail.com>
2015-08-23 17:50:57 +0300

    Some FuzzyContains Tests

--- SearcherTests/Model/SearchableExtensionTests.cs
+++ SearcherTests/Model/SearchableExtensionTests.cs
@@ -45,10 +45,10 @@ namespace Searcher.Model.Tests
             SearchableOneResultSearch s1 = new SearchableOneResultSearch();
             Assert.AreEqual(0, s1.Search(new string[] { }));
             Assert.AreEqual(1, s1.Search(new string[] { "a", }));
-            Assert.AreEqual(2, s1.Search(new string[] { "a", "b"}));
+            Assert.AreEqual(2, s1.Search(new string[] { "a", "b" }));
             Assert.AreEqual(3, s1.Search(new string[] { "a", "b", "c" }));
             Assert.AreEqual(4, s1.Search(new string[] { "a", "b", "c", "d" }));
-            Assert.AreEqual(5, s1.Search(new string[] { "a", "b", "c", "d", "e"}));
+            Assert.AreEqual(5, s1.Search(new string[] { "a", "b", "c", "d", "e" }));
 
             SearchableZeroResultSearch s2 = new SearchableZeroResultSearch();
             Assert.AreEqual(0, s2.Search(new string[] { }));
@@ -56,7 +56,7 @@ namespace Searcher.Model.Tests
             Assert.AreEqual(0, s2.Search(new string[] { "a", "b" }));
             Assert.AreEqual(0, s2.Search(new string[] { "a", "b", "c" }));
             Assert.AreEqual(0, s2.Search(new string[] { "a", "b", "c", "d" }));
-            Assert.AreEqual(0, s2.Search(new string[] { "a", "b", "c", "d", "e"}));
+            Assert.AreEqual(0, s2.Search(new string[] { "a", "b", "c", "d", "e" }));
         }
 
         [TestMethod()]
@@ -77,6 +77,47 @@ namespace Searcher.Model.Tests
             Assert.IsFalse(s2.SearchAll(new string[] { "a", "b", "c" }));
             Assert.IsFalse(s2.SearchAll(new string[] { "a", "b", "c", "d" }));
             Assert.IsFalse(s2.SearchAll(new string[] { "a", "b", "c", "d", "e" }));
+        }
+
+        [TestMethod()]
+        public void FuzzyContains_Contains_Test()
+        {
+            string stringToSearch = "abcdefghi";
+
+            Assert.IsTrue(stringToSearch.FuzzyContains("a"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("i"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("d"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("ab"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("bc"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("abc"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("cde"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("ghi"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("defg"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("abcde"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("bcdefgh"));
+            Assert.IsTrue(stringToSearch.FuzzyContains(stringToSearch));
+        }
+
+        [TestMethod()]
+        public void FuzzyContains_UnicodeContains_Test()
+        {
+            string stringToSearch = "אבגדהו";
+
+            Assert.IsTrue(stringToSearch.FuzzyContains("בגד"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("אב"));
+            Assert.IsTrue(stringToSearch.FuzzyContains("דה"));
+            Assert.IsTrue(stringToSearch.FuzzyContains(stringToSearch));
+        }
+
+        [TestMethod()]
+        public void FuzzyContains_EmptyTerms_Test()
+        {
+            string testString = "abcdefghi";
+            string emptyString = "";
+
+            Assert.IsTrue(testString.FuzzyContains(emptyString));
+            Assert.IsTrue(emptyString.FuzzyContains(emptyString));
+            Assert.IsFalse(emptyString.FuzzyContains(testString));
         }
     }
 }
